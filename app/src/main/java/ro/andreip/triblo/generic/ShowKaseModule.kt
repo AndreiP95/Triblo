3fb811e3c9025c9a768279104bb899ed362f42f5
AndreiP95 <andreipancu95@gmail.com>
2024-08-12 13:42:44 +0300

    Integrate showkase lib

---
+++ app/src/main/java/ro/andreip/triblo/generic/ShowKaseModule.kt
@@ -0,0 +1,7 @@
+package ro.andreip.triblo.generic
+
+import com.airbnb.android.showkase.annotation.ShowkaseRoot
+import com.airbnb.android.showkase.annotation.ShowkaseRootModule
+
+@ShowkaseRoot
+class ShowKaseModule: ShowkaseRootModule
